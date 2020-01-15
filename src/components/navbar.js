f6609e7f0c30f773f19b4074ef32c7d526be3dce
smittey <andy_smithy@hotmail.co.uk>
2020-01-15 21:25:27 +0000

    Nav image working all but mobile index

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 import { FaBars } from 'react-icons/fa';
@@ -21,15 +21,54 @@ const Navbar = ({ isIndex }) => {
   );
 
   const [menuState, setMenuState] = useState('closed');
+  const [backgroundPositionStyle, setBackgroundPositionStyle] = useState('');
+
 
   const navToggle = () => {
     setMenuState(menuState === 'closed' ? 'open' : 'closed');
   };
 
+  useEffect(() => {
+    let style;
+
+    if(isIndex) { // index
+      if(menuState === 'open') { // mobile
+        style = "bottom";
+        console.log('style: ', style)
+
+      } else if(menuState === 'closed'){
+        style = "center bottom -100px";
+        console.log('style: ', style)
+
+      } else {
+        style = "left bottom";
+        console.log('style: ', style)
+
+      }
+    } else {
+      if(menuState === 'open') {
+        style = "bottom -100px";
+        console.log('style: ', style)
+
+      } else {
+        style = "center bottom -100px";
+        console.log('style: ', style)
+
+      }
+     
+    }
+    setBackgroundPositionStyle(style);
+
+    console.log('backgroundPositionstyle: ', backgroundPositionStyle)
+  }, [menuState]);
+
+
+  
+
   return (
     <nav className="rightNav" style={{ minWidth: isIndex ? '40%' : 'fit-content' }}>
 
-      <button type="button" className="menuIcon" onClick={navToggle} tabIndex="0" onKeyDown={navToggle}>
+      <button type="button" className="menuIcon" onClick={() => navToggle()} tabIndex="0" onKeyDown={navToggle}>
         <IconContext.Provider value={{ className: (menuState === 'closed') ? 'icon' : 'iconClicked' }}>
           <FaBars />
         </IconContext.Provider>
@@ -56,7 +95,9 @@ const Navbar = ({ isIndex }) => {
             style={{
               height: '100%',
               backgroundSize: 'contain',
-              backgroundPosition: isIndex ? 'left bottom' : 'center bottom -100px',
+              // backgroundPosition: "bottom",
+              // backgroundPosition: isIndex ? 'left bottom' : 'center bottom -100px',
+              backgroundPosition: backgroundPositionStyle,
             }}
             fluid={contentfulAsset.sizes}
           />
