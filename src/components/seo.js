8a0d7db33748b9504f105a2145b33cbcced35841
smittey <andy_smithy@hotmail.co.uk>
2021-02-14 17:47:44 +0000

    Changing twitter card from summary to large

--- src/components/seo.js
+++ src/components/seo.js
@@ -87,7 +87,7 @@ function SEO({
         },
         {
           name: 'twitter:card',
-          content: 'summary',
+          content: 'summary_large_image',
         },
         {
           name: 'twitter:creator',
